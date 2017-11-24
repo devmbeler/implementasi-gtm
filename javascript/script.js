3763d976d45816771e60eb204e43d1ac5a6f4096
wnua.dev <wnua.dev@gmail.com>
2017-11-24 09:39:28 +0700

    added name and age variable

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,16 @@
 var __gtmHelper = {
+    name : this.getName(),
+    age : this.getAge(),
+    getName: function(){
+        console.log("getName function is fired");
+        return "wahyu albab";
+    },
+    getAge: function(){
+        console.log("getAge function is fired");
+        return 24;
+    },
     printMessage : function(message){
         console.log(message);
     }
 }
+__gtmHelper.printMessage("fired from script.js");
